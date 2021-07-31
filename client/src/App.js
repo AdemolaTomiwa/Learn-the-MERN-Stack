8b815e78c555d0a36fa4bb1a9402700d80df3180
Tomiwa Adelae <tommedia6001@gmail.com>
2021-07-31 17:33:47 -0200

    Initial Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Tomiwa</h1>
-    </div>
-  );
+   return (
+      <div className='App'>
+         <AppNavbar />
+         <ShoppingList />
+      </div>
+   );
 }
 
 export default App;
